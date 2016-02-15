9192eeac492b8f308f1ab1aa50219784bff24760
EarvinKayonga <demoneartin@gmail.com>
2016-02-15 20:47:40 +0100

    Normes

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
-#include <string>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include "Person/person.h"
-#include "Vector/vector.h"
+#include	<iostream>
+#include	<string>
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<time.h>
+#include	"Person/person.h"
+#include	"Vector/vector.h"
 
 
 using namespace std;
 
-int     main()
+int		main()
 {
-  Person Earvin("Earvin", "Kayonga", 23);
-  Person Chris("Christelle", "Fremy", 22);
-  Vector zero(0, 0, 0, 0);
+  Person	Earvin("Earvin", "Kayonga", 23);
+  Person	Chris("Christelle", "Fremy", 22);
+  Vector	zero(0, 0, 0, 0);
 
   zero.printf(); 
   Earvin.say("hi");
--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,19 +1,19 @@
-#include <cassert>
-#include <iostream>
-#include "../../src/Vector/vector.h"
-#include "../../src/Person/person.h"
+#include	<cassert>
+#include	<iostream>
+#include	"../../src/Vector/vector.h"
+#include	"../../src/Person/person.h"
 
 using namespace std;
 
-const string red("\033[0;31m");
-const string green("\033[1;32m");
-const string yellow("\033[1;33m");
-const string cyan("\033[0;36m");
-const string magenta("\033[0;35m");
-const string reset("\033[0m");
+const string	red("\033[0;31m");
+const string	green("\033[1;32m");
+const string	yellow("\033[1;33m");
+const string	cyan("\033[0;36m");
+const string	magenta("\033[0;35m");
+const string	reset("\033[0m");
 
 
-int main(){
+int		main(){
   Person Chris("Christelle", "Fremy", 23);
 
   cout << "Testing Object at initialization";
