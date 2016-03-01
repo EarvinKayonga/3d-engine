5d11d62ff271844cf6793dde6a0c45f003f82f4d
EarvinKayonga <demoneartin@gmail.com>
2016-03-01 19:15:51 +0100

    Comments in Vector Class

--- src/Vector/vector.h
+++ src/Vector/vector.h
@@ -8,21 +8,36 @@ class Vector{
       private:
         double	 coords[4];
       public:
+
+	/*
+	* Initializer with an Array
+	*/
+	
 	Vector(double tab[4]){
 	  for (int nb = 0;  nb < 4; nb++){
 	    coords[nb] = tab[nb];
 	  }
 	};
 
+	/*
+	 * Initializer with 4 values
+	 */
 	Vector(double one, double two, double viva, double lalgeria){
 	  coords[0] = one;
 	  coords[1] = two;
 	  coords[2] = viva;
 	  coords[3] = lalgeria;
 	};
-	
+
+	/*
+	 *  Print and toString methods
+	 */
 	void     printf();
 	string   toString();
+
+	/*
+	 * Getters and setters
+	 */
 	double   get(const int) const;
 	int      set(const int, const double);
 	void     set_x(double);
@@ -34,8 +49,15 @@ class Vector{
 	double   y();
 	double   z();
 	double   d();
+
+	/*
+	 *  Distance from (0,0,0) in (x, y, z) 
+	 */
 	double   length();
 
+	/*
+	 * Operator Overloading
+	 */
 	Vector  operator+(const Vector&);
 	Vector  operator*(const double&);
 	double  operator*(const Vector&); //Dot Product 
@@ -47,6 +69,9 @@ class Vector{
 	Vector& operator-=(const Vector& right);
 	Vector& operator*=(const double& right);
 
+	/*
+	 * Default Constructor
+	 */
 	Vector(){
 	  for (int i = 0; i < 4; i++){
 	    coords[i] = 0;
